a6ba8997750cb78294824ccd36711d110d182036
Cauã Rodrigues Gonzatto <cauarsastre@gmail.com>
2026-04-16 17:18:18 -0300

    feat: implement REST Controller for full Asset CRUD

---
+++ src/main/java/com/cauarodrigues/assetmasterbackend/controllers/AssetController.java
@@ -0,0 +1,46 @@
+package com.cauarodrigues.assetmasterbackend.controllers;
+
+import com.cauarodrigues.assetmasterbackend.entities.Asset;
+import com.cauarodrigues.assetmasterbackend.repositories.AssetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/assets")
+public class AssetController {
+    @Autowired
+    private AssetRepository repository;
+
+    @GetMapping
+    public List<Asset> getAllAssets() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Asset createAsset(@RequestBody Asset asset) {
+        return repository.save(asset);
+    }
+
+    // Deletar um ativo
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAsset(@PathVariable UUID id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // Atualizar um ativo (mudar dono ou status)
+    @PutMapping("/{id}")
+    public ResponseEntity<Asset> updateAsset(@PathVariable UUID id, @RequestBody Asset details) {
+        return repository.findById(id).map(asset -> {
+            asset.setName(details.getName());
+            asset.setCurrentOwner(details.getCurrentOwner());
+            asset.setStatus(details.getStatus());
+            Asset updated = repository.save(asset);
+            return ResponseEntity.ok(updated);
+        }).orElse(ResponseEntity.notFound().build());
+    }
+}
